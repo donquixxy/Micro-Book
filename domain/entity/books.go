467d2647654a4a74c02c9f3bff01705c18cf05ed
donquixxy <agusary726@gmail.com>
2023-10-13 13:18:37 +0800

    fix : books field

--- domain/entity/books.go
+++ domain/entity/books.go
@@ -1,3 +1,18 @@
 package entity
 
-type Books struct{}
+import "time"
+
+type Books struct {
+	ID         string    `json:"id" gorm:"column:id;primaryKey"`
+	IDCategory string    `json:"id_category" gorm:"column:id_category;index"`
+	IDGenre    string    `json:"id_genre" gorm:"column:id_genre;index"`
+	Title      string    `json:"title" gorm:"column:title"`
+	ISBN       string    `json:"isbn" gorm:"column:isbn"`
+	Price      float64   `json:"price" gorm:"column:price"`
+	CreatedAt  time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt  time.Time `json:"updated_at" gorm:"column:updated_at"`
+}
+
+func (*Books) TableName() string {
+	return "book"
+}
